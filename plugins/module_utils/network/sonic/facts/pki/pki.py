53bb82853e86d889cbbbe8b8930dfdae57a8c289
sdl <sdklfgj@zdjklfcom>
2023-06-28 18:10:43 +0000

    wip

--- plugins/module_utils/network/sonic/facts/pki/pki.py
+++ plugins/module_utils/network/sonic/facts/pki/pki.py
@@ -65,7 +65,7 @@ class PkiFacts(object):
         if resources.get('openconfig-pki:pki') \
                 and resources.get('openconfig-pki:pki').get('security-profiles') \
                 and resources.get('openconfig-pki:pki').get('security-profiles').get('security-profile') :
-            objs['security-profiles'] = resources.get('openconfig-pki:pki').get('security-profiles').get('security-profile')
+            objs['security-profiles'] = resources.get('openconfig-pki:pki').get('security-profiles')#.get('security-profile')
         if resources.get('openconfig-pki:pki') \
                 and resources.get('openconfig-pki:pki').get('trust-stores') \
                 and resources.get('openconfig-pki:pki').get('trust-stores').get('trust-store') :
